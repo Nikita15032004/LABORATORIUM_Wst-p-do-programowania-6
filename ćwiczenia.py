5cb54dd2ad4fb65f54c5ff75ea8cf17a99f785ee
Nikita15032004 <61089069+Nikita15032004@users.noreply.github.com>
2024-11-24 12:17:42 +0100

    Add files via upload

---
+++ ćwiczenia.py
@@ -0,0 +1,15 @@
+#A
+from modules import f_mat
+
+print(f_mat.kwadrat(10))
+print(f_mat.szescian(3))
+print(f_mat.dodaj(10, 5))
+
+
+print("------------------------------------------------")
+#B
+from modules.f_mat import kwadrat, szescian, dodaj
+
+print(kwadrat(10))
+print(szescian(3))
+print(dodaj(10, 5))
